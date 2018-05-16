809cf3e6492f777fadfee5263f0d3b0bd7e4d6d1
Андрей Аракельянц <arakelyants.andrey@yandex.ru>
2018-05-16 12:56:59 +0300

    Make the first version of script and tests.

--- format_price.py
+++ format_price.py
@@ -1,5 +1,30 @@
+import argparse
+
+
+def get_console_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('price', help='Enter a price.')
+    arguments = parser.parse_args()
+    return arguments
+
+
 def format_price(price):
-    pass
+    if type(price) == bool:
+        return None
+    try:
+        real_num_price = float(price)
+    except (TypeError, ValueError):
+        return None
+    rounded_price = round(real_num_price, 2)
+    if rounded_price.is_integer():
+        formatted_price = '{:,.0f}'.format(rounded_price).replace(',', ' ')
+    else:
+        formatted_price = '{:,}'.format(rounded_price).replace(',', ' ')
+    return formatted_price
+
 
 if __name__ == '__main__':
-    pass
+    console_arguments = get_console_arguments()
+    price = console_arguments.price
+    formatted_price = format_price(price)
+    print(formatted_price)
--- tests.py
+++ tests.py
@@ -1,1 +1,63 @@
-# TODO place your tests here
+import unittest
+from format_price import format_price
+
+
+class TestFormatPriceFunction(unittest.TestCase):
+
+    def test_real_num_string(self):
+        price = '1234.567'
+        self.assertEqual(format_price(price), '1 234.57')
+
+    def test_integer_num_string(self):
+        price = '1234'
+        self.assertEqual(format_price(price), '1 234')
+
+    def test_real_num(self):
+        price = 1234.567
+        self.assertEqual(format_price(price), '1 234.57')
+
+    def test_integer_num(self):
+        price = 1234
+        self.assertEqual(format_price(price), '1 234')
+
+    def test_zero(self):
+        self.assertEqual(format_price(0), '0')
+
+    def test_negative_number(self):
+        price = -12345.678
+        self.assertEqual(format_price(price), '-12 345.68')
+
+    def test_many_fraction_zeros(self):
+        price = '12345.0000001'
+        self.assertEqual(format_price(price), '12 345')
+
+    def test_few_fraction_zeros(self):
+        price = '12345.00'
+        self.assertEqual(format_price(price), '12 345')
+
+    def test_not_digit_string(self):
+        price = '123abcs'
+        self.assertIsNone(format_price(price))
+
+    def test_empty_string(self):
+        price = ''
+        self.assertIsNone(format_price(price))
+
+    def test_list(self):
+        price = [1, 2, 3]
+        self.assertIsNone(format_price(price))
+
+    def test_complex_number(self):
+        price = '1+2j'
+        self.assertIsNone(format_price(price))
+
+    def test_bool(self):
+        price = False
+        self.assertIsNone(format_price(price))
+
+    def test_none(self):
+        self.assertIsNone(format_price(None))
+
+
+if __name__ == '__main__':
+    unittest.main()
